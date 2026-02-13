c707d0da087942c94aee26c507d47f3914094523
kelly <kelly_yang@berkeley.edu>
2026-02-13 14:45:13 -0800

    feat: add floating navigation dots

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import CrystalCanvas from "@/components/Crystal/CrystalCanvas";
+import Navigation from "@/components/Navigation";
 import HeroSection from "@/components/sections/HeroSection";
 import AboutSection from "@/components/sections/AboutSection";
 import ExperienceSection from "@/components/sections/ExperienceSection";
@@ -10,6 +11,7 @@ export default function Home() {
   return (
     <main className="relative">
       <CrystalCanvas />
+      <Navigation />
       <HeroSection />
       <AboutSection />
       <ExperienceSection />
